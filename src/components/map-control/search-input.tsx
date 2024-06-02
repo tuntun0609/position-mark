aa7b0e8436139b23ab7fc2fadd5dff0e4cf2e6dd
tuntun <760523867@qq.com>
2024-06-02 22:36:32 +0800

    feat: search

--- src/components/map-control/search-input.tsx
+++ src/components/map-control/search-input.tsx
@@ -1,23 +1,20 @@
 'use client'
 
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from '@/components/ui/command'
-import { useState } from 'react'
+import { useClickAway } from 'react-use'
+import { useRef, useState } from 'react'
 import { useMap } from '../map-context'
+import { Button } from '../ui/button'
+import { Search } from 'lucide-react'
 
 export const SearchInput = () => {
   const [query, setQuery] = useState('')
   const [siteList, setSiteList] = useState<any[]>([])
-  const { hwService } = useMap()
+  const { map, hwService } = useMap()
+  const [searchResultOpen, setSearchResultOpen] = useState(false)
+  const ref = useRef(null)
+  useClickAway(ref, () => {
+    setSearchResultOpen(false)
+  })
 
   const onSearch = async () => {
     try {
@@ -34,9 +31,62 @@ export const SearchInput = () => {
     } catch (error) {}
   }
 
+  const onClickSite = (site: any) => {
+    map?.setView([site.location.lat, site.location.lng], 23)
+  }
+
   return (
-    <div className="absolute top-2 left-2 z-[9999]">
-      <Command className="w-96">
+    <div ref={ref} className="absolute top-2 left-2 z-[9999] w-[400px]">
+      <div className="relative">
+        <label htmlFor="Search" className="sr-only">
+          {' '}
+          Search{' '}
+        </label>
+
+        <input
+          value={query}
+          onChange={(e) => {
+            if (siteList.length) {
+              setSiteList([])
+            }
+            setQuery(e.target.value)
+          }}
+          onFocus={() => {
+            setSearchResultOpen(true)
+          }}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              onSearch()
+            }
+          }}
+          type="text"
+          id="Search"
+          placeholder="在这里搜索地址哦"
+          className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm focus:ring-primary"
+        />
+
+        <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+          <Button variant="outline" className="p-0 w-8 h-8" onClick={onSearch}>
+            <span className="sr-only">Search</span>
+            <Search className="w-4 h-4" />
+          </Button>
+        </span>
+      </div>
+      {searchResultOpen && siteList.length > 0 && (
+        <div className="mt-2 max-h-96 z-[9999] bg-white rounded-xl p-2">
+          <div className=" max-h-80 overflow-y-auto">
+            {siteList.map((site) => (
+              <div
+                className="rounded-xl h-10 overflow-hidden whitespace-nowrap text-sm flex items-center px-4 cursor-pointer hover:bg-gray-100"
+                key={site.siteId}
+                onClick={() => onClickSite(site)}>
+                {site.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {/* <Command className="w-96" shouldFilter={false}>
         <CommandInput
           value={query}
           onValueChange={(search) => {
@@ -46,12 +96,23 @@ export const SearchInput = () => {
           placeholder="请输入想要查询的地址"
         />
         <CommandList>
-          {/* <CommandEmpty>没有这个地址哦</CommandEmpty> */}
           {siteList.map((site) => (
-            <CommandItem key={site.siteId}>{site.formatAddress}</CommandItem>
+            <CommandItem
+              className="h-10 overflow-hidden whitespace-nowrap text-sm"
+              key={site.siteId}
+              value={site.siteId}
+              onSelect={() => {
+                map?.setView(
+                  [site.viewport.northeast.lat, site.viewport.northeast.lng],
+                  16
+                )
+              }}>
+              {site.formatAddress}
+              <FontAwesomeIcon icon={faLocationDot} />
+            </CommandItem>
           ))}
         </CommandList>
-      </Command>
+      </Command> */}
     </div>
   )
 }
